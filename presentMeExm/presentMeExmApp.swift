ef95c38c4b265e9a218b3b83ea4db01eb9762acb
razanalsunaidi <razanalsunedi@gmail.com>
2024-09-29 14:47:54 +0300

    Initial Commit

---
+++ presentMeExm/presentMeExmApp.swift
@@ -0,0 +1,17 @@
+//
+//  presentMeExmApp.swift
+//  presentMeExm
+//
+//  Created by Razan Alsunaidi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct presentMeExmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
